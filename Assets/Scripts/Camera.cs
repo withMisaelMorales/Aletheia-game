34bbd676681fb2d616065097bc1c32976a87a607
Misael Morales <248335657+withMisaelMorales@users.noreply.github.com>
2026-01-01 18:11:41 -0600

    mod: camera tracking

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CamaraSigue : MonoBehaviour
+{
+    [SerializeField] private Transform objetivo; // Tu Player
+    [SerializeField] private float suavizado = 0.125f; // Cuanto más bajo, más elástica la cámara    
+    [SerializeField] private Vector3 desfasePersonalizado = new Vector3(0, 2, -10);
+
+    void LateUpdate()
+    {
+        if (objetivo != null)
+        {
+            // Sumamos el desfase a la posición del jugador
+            // Pero mantenemos la X de la cámara fija
+            float xFija = transform.position.x;
+            float ySeguimiento = objetivo.position.y + desfasePersonalizado.y;
+            float zSeguimiento = desfasePersonalizado.z;
+
+            Vector3 posicionDeseada = new Vector3(xFija, ySeguimiento, zSeguimiento);
+            
+            transform.position = Vector3.Lerp(transform.position, posicionDeseada, suavizado);
+        }
+    }
+}
