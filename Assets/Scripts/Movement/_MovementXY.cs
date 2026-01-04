47e0ac0e3e1130f40948830779789577480e5cdf
Misael Morales <248335657+withMisaelMorales@users.noreply.github.com>
2026-01-03 22:41:32 -0600

    refactoring: movement on x and y

--- Assets/Scripts/Movement/_MovementXY.cs
+++ Assets/Scripts/Movement/_MovementXY.cs
@@ -2,16 +2,37 @@ using UnityEngine;
 
 public class _MovementXY : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public enum MovementDirection { Horizontal, Vertical }
+
+    [Header("Configuración de Selección")]
+    [SerializeField] private MovementDirection direccion = MovementDirection.Horizontal;
+
+    [Header("Ajustes de Movimiento")]
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float distance = 3f;
+
+    private Vector3 startPos;
+
     void Start()
     {
-        
+        // Guardamos la posición inicial como punto de anclaje
+        startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculamos el valor de oscilación
+        float movement = Mathf.PingPong(Time.time * speed, distance) - (distance / 2f);
+
+        // Aplicamos el movimiento basado en la elección del Enum
+        if (direccion == MovementDirection.Horizontal)
+        {
+            transform.position = new Vector3(startPos.x + movement, startPos.y, startPos.z);
+        }
+        else if (direccion == MovementDirection.Vertical)
+        {
+            transform.position = new Vector3(startPos.x, startPos.y + movement, startPos.z);
+        }
     }
 }
 
